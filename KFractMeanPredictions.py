3cfebbb42b28f61d48b8809453b0fa557b1cec88
rnurkhamitov <rnurkhamitov@dataarmor.ru>
2021-11-26 19:58:51 +0300

    Recommendation predict functions

--- KFractMeanPredictions.py
+++ KFractMeanPredictions.py
@@ -3,27 +3,60 @@ import numpy as np
 from DataPreparing import prepare_data
 from RMSE import rmse
 
-
-# TODO: Хабиб, тут берешь, значит, ту статью,
-#  и по разделу "Рекомендации на основе средних оценок пользователей и матрицы “похожести”"
-#  вот сюда пишешь код в две функции ниже.
-#  Только помни, что переменные user_similarity, item_similarity,
-#  n_users, n_movies, train_data_matrix и test_data_matrix нужно
-#  вызывать как ключи словаря: data["user_similarity"], data["n_users"]...
-
+#Рекомендации на основе средних оценок пользователей и матрицы “похожести”
 
 def k_fract_mean_predict(top, data):
+    top_similar = np.zeros((data["n_users"], top))
+    pred = np.zeros((data["n_users"], data["n_movies"]))
+    abs_sim = np.abs(data["user_similarity"])
 
-    pred = np.zeros((data["n_movies"], data["n_users"]))
+    for i in range(data["n_users"]):
+        user_sim = data["user_similarity"][i]
+        top_sim_users = user_sim.argsort()[1:top + 1]
+
+        for j in range(top):
+            top_similar[i, j] = top_sim_users[j]
+
+    for i in range(data["n_users"]):
+        indexes = top_similar[i].astype(np.int)
+        numerator = data["user_similarity"][i][indexes]
+
+        mean_rating = np.array([x for x in data["train_data_matrix"][i] if x > 0]).mean()
+        diff_ratings = data["train_data_matrix"][indexes] - data["train_data_matrix"][indexes].mean()
+        numerator = numerator.dot(diff_ratings)
+        denominator = abs_sim[i][top_similar[i].astype(np.int)].sum()
+
+        pred[i] = mean_rating + numerator / denominator
 
     return pred
 
 
 def k_fract_mean_predict_item(top, data):
-
     pred = np.zeros((data["n_movies"], data["n_users"]))
+    top_similar = np.zeros((data["n_movies"], top))
+    abs_sim = np.abs(data['item_similarity'])
 
-    return pred
+    for i in range(data["n_movies"]):
+        movies_sim = data['item_similarity'][i]
+        top_sim_movies = movies_sim.argsort()[1:top + 1]
+
+        for j in range(top):
+            top_similar[i, j] = top_sim_movies.T[j]
+
+    for i in range(n_users):
+        indexes = top_similar[i].astype(np.int)
+        numerator = data['item_similarity'][i][indexes]
+
+        diff_ratings = data['train_data_matrix'].T[indexes] - data['train_data_matrix'].T[indexes].mean()
+        numerator = numerator.dot(diff_ratings)
+        denominator = abs_sim[i][top_similar[i].astype(np.int)].sum()
+        denominator = denominator if denominator != 0 else 1
+        mean_rating = np.array([x for x in data['train_data_matrix'].T[i] if x > 0]).mean()
+        mean_rating = 0 if np.isnan(mean_rating) else mean_rating
+
+        pred[i] = mean_rating + numerator / denominator
+
+    return pred.T
 
 
 prepared_data = prepare_data(path='S:/Проекты 5 курс/Python/Датасеты/ratings.csv',
--- KFractPredictions.py
+++ KFractPredictions.py
@@ -3,25 +3,53 @@ import numpy as np
 from DataPreparing import prepare_data
 from RMSE import rmse
 
-
-# TODO: Короче, Меченый, берешь, значит, ту статью,
-#  и по разделу "Рекомендации с учётом средних оценок похожих пользователей"
-#  вот сюда пишешь код в две функции ниже.
-#  Только помни, что переменные user_similarity, item_similarity,
-#  n_users, n_movies, train_data_matrix и test_data_matrix нужно
-#  вызывать как ключи словаря: data["user_similarity"], data["n_users"]...
-
+#Рекомендации с учётом средних оценок похожих пользователей
 
 def k_fract_predict(top, data):
-    pred = np.zeros((data["n_movies"], data["n_users"]))
+    pred = np.zeros((data["n_users"], data["n_movies"]))
+    top_similar = np.zeros((data["n_users"], top))
+    abs_sim = np.abs(data["user_similarity"])
+
+    for i in range(data["n_users"]):
+        user_sim = data["user_similarity"][i]
+        top_sim_users = user_sim.argsort()[1:top + 1]
+
+        for j in range(top):
+            top_similar[i, j] = top_sim_users[j]
+
+    for i in range(data["n_users"]):
+        indexes = top_similar[i].astype(np.int)
+        numerator = data["user_similarity"][i][indexes]
+        product = numerator.dot(data['train_data_matrix'][indexes])
+        denominator = abs_sim[i][top_similar[i].astype(np.int)].sum()
+
+        pred[i] = product / denominator
 
     return pred
 
 
 def k_fract_predict_item(top, data):
     pred = np.zeros((data["n_movies"], data["n_users"]))
+    top_similar = np.zeros((data["n_movies"], top))
+    abs_sim = np.abs(data['item_similarity'])
 
-    return pred
+    for i in range(data["n_movies"]):
+        movies_sim = data['item_similarity'][i]
+        top_sim_movies = movies_sim.argsort()[1:top + 1]
+
+        for j in range(top):
+            top_similar[i, j] = top_sim_movies.T[j]
+
+    for i in range(data["n_users"]):
+        indexes = top_similar[i].astype(np.int)
+        numerator = data['item_similarity'][i][indexes]
+        product = numerator.dot(data['train_data_matrix'].T[indexes])
+        denominator = abs_sim[i][indexes].sum()
+        denominator = denominator if denominator != 0 else 1
+
+        pred[i] = product / denominator
+
+    return pred.T
 
 
 prepared_data = prepare_data(path='S:/Проекты 5 курс/Python/Датасеты/ratings.csv',
